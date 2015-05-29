328beaf43459e49b5f6c3ec5c032927596e63f95
ZAE <zestela@gmail.com>
2015-05-29 14:58:21 -0700

    Performance optimizations
    Added threading to clear graph cache periodically in order to maximize
    sample plotting rate.
    Added a graph of time per sample process after 200 samples are plotted.
    I used this feedback to verify sample rate optimization efforts.

--- PlotNow.py
+++ PlotNow.py
@@ -1,5 +1,7 @@
+
+
 import matplotlib.pyplot as plt #import matplotlib library
-from drawnow import *
+import matplotlib.figure as fig
 import time
 import matplotlib.lines as mlines
 print 'setting up...'
@@ -8,37 +10,49 @@ YVals =[]
 ZVals = []
 #plt.ion() #Tell matplotlib you want interactive mode to plot live data
 cnt=0
-
+Clist = []
 from Arduino_Monitor import SerialData as DataGen
 
 data = DataGen()
-time.sleep(3)
+time.sleep(2)
+
+
 
 def makeFig(): 
-                              
-    plt.title('My Live Acc Sensor Data')    
-    plt.grid(True) 
-    plt.ylim( -40000, 40000)                              
- 
-    plt.ticklabel_format(useOffset=False)                          
-    
-    plt.plot(XVals, 'g-')
-    plt.plot(YVals, 'b-')       
-    plt.plot(ZVals, 'r-')
-    green_line = mlines.Line2D([], [], color='green',
-                          markersize=10, label='X')
-    blue_line = mlines.Line2D([], [], color='blue',
-                          markersize=10, label='Y')
 
-    red_line = mlines.Line2D([], [], color='red',
-                          markersize=10, label='Z')
-    plt.legend(handles=[green_line, blue_line,red_line],loc=2)
+        #plt.clf()
+#        plt.title('Live Sensor Data')    
+#        plt.grid(True) 
+        plt.ylim( -40000, 40000)  
+        
+                           
+        plt.xlim(cnt-40,cnt)
+
+        plt.ticklabel_format(useOffset=False)  
+#        green_line = mlines.Line2D([], [], color='green',
+#                              markersize=10, label='X')
+#        blue_line = mlines.Line2D([], [], color='blue',
+#                              markersize=10, label='Y')
+#        red_line = mlines.Line2D([], [], color='red',
+#                              markersize=10, label='Z')
+#        plt.legend(handles=[green_line, blue_line,red_line],loc=2)
+        
+              
+        plt.plot(Clist,XVals, 'g-')
+        plt.plot(Clist,YVals, 'b-')       
+        plt.plot(Clist,ZVals, 'r-')
+    
+        
+        plt.draw()
+    
+    
 
 Xoff = 0
 Yoff = 0
 Zoff = 0
 
 def cal():
+   
     print 'calibrating sensors...'
     global Xoff
     global Yoff
@@ -48,46 +62,92 @@ def cal():
     Zcal = []
     for i in range(10):
         try:
-            time.sleep(.25)  
+            time.sleep(.05)  
             dataArray = data.next().split('\t')
             Xcal.append(int(dataArray[0]) )
             Ycal.append(int(dataArray[1]) )          
             Zcal.append(int(dataArray[2]) ) 
         except ValueError:
             print "Value Error:" % dataArray
-               
-    
+                  
         
     Xoff =  sum(Xcal)/len(Xcal)
     Yoff =  sum(Ycal)/len(Ycal)          
     Zoff =  sum(Zcal)/len(Zcal)
-    
+  
 
-cal()
-while True: 
-    xList = []
-    yList = []
-    zList = []
-    
-    
-    try:   
 
-        dataArray = data.next().split('\t')
-
-        XVals.append(int(dataArray[0]) - Xoff)          
-        YVals.append(int(dataArray[1]) - Yoff)
-        ZVals.append(int(dataArray[2]) - Zoff)                 
-        drawnow(makeFig)                   
-                    
-        cnt=cnt+1
-        if(cnt>30):                          
-            ZVals.pop(0)  
-            YVals.pop(0)
-            XVals.pop(0)                  
+def grab():
+        
+        global cnt
+        global Clist
+        global ZVals
+        global YVals
+        global XVals
+        dataArray = data.next().split('\t')      
+        try:
+                     
+            XVals.append(int(dataArray[0]) - Xoff)          
+            YVals.append(int(dataArray[1]) - Yoff)
+            ZVals.append(int(dataArray[2]) - Zoff)                 
+    
             
-#        if cnt > 200:
-#            data.__del__()
-#            exit()
-    except Exception as e:
-        print e, dataArray
+                               
+            cnt=cnt+1
+            Clist.append(cnt)
+#            if(cnt>50):                                         
+
+#                
+            if cnt > 200:
+                global a
+                a = False
+#                data.__del__()
+#                exit()
+            
+        except Exception as e:
+            print e, dataArray
+
+def clear():
+    
+    while True:
+        time.sleep(3)
+        plt.clf()
+        
+    
+def main():
+    
+    from threading import Thread
+    timer = []
+    a= True
+    cal()
+    global XVals
+    global YVals
+    global ZVals
+    global Clist
+    
+    Thread(target=clear).start()
+
+    while a:
+        start = time.time()
+        grab()
+        
+        makeFig()
+        end = time.time()
+
+        timer.append(end-start)
+        if cnt > 150:
+            a = False
+        
+            
+
+            
+            
+    plt.clf()
+    plt.plot(timer)
+    plt.grid(True)
+    plt.title('ave time: %s' % (sum(timer)/len(timer)))
+    plt.show()
+        
+        
+main()
         
